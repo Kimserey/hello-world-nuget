e88e15d9e70d762de2ad3c811cbcdc59b393f66e
Kimserey <kimserey.lam@groomgy.com>
2018-08-28 09:13:20 +0100

    Added console app to test package

---
+++ Groomgy.TestConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Groomgy.HelloWorld;
+
+namespace Groomgy.TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new HelloWorldService().Say());
+            Console.ReadLine();
+        }
+    }
+}
