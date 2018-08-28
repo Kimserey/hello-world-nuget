807b65c8b67f86722917269cc8d4464ab0173e25
Kimserey <kimserey.lam@groomgy.com>
2018-08-28 14:27:40 +0100

    added pack target

--- build.fsx
+++ build.fsx
@@ -5,21 +5,38 @@ open Fake.IO
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
+let configuration =
+    DotNet.BuildConfiguration.fromEnvironVarOrDefault "BUILD_CONFIGURATION" DotNet.BuildConfiguration.Debug
+
 Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
+    !! "**/bin"
+    ++ "**/obj"
+    ++ "artifacts"
     |> Shell.cleanDirs 
 )
 
 Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+    !! "**/*.*proj"
+    |> Seq.iter (DotNet.build (fun opts -> { opts with Configuration = configuration }))
+)
+
+Target.create "Pack" (fun _ ->
+    DotNet.pack 
+        (fun opts -> 
+            { opts with 
+                Configuration = configuration
+                OutputPath = Some "../artifacts/Groomgy.HelloWorld"
+                VersionSuffix = Some ""
+                NoBuild = true
+            })
+        "./Groomgy.HelloWorld"
 )
 
 Target.create "All" ignore
 
 "Clean"
   ==> "Build"
+  ==> "Pack"
   ==> "All"
 
-Target.runOrDefault "All"
+Target.runOrDefault "Build"
