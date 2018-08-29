bc43e08504c2ddf3950605d6d6d3d1b03341e40b
Kimserey <kimserey.lam@groomgy.com>
2018-08-29 22:31:42 +0100

    Set package build with semver

--- build.fsx
+++ build.fsx
@@ -5,6 +5,21 @@ open Fake.IO
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
+module GitVersion =
+    let exec args =
+        Process.execWithResult
+            (fun info -> { info with FileName = "gitversion"; Arguments = args })
+            (System.TimeSpan.FromMinutes 2.)
+
+    let fullSemVer () =
+        let result = exec "/showvariable fullsemver"
+        result.Messages |> List.head
+
+    let semVer () =
+        let result = exec "/showvariable semver"
+        result.Messages |> List.head
+
+
 let configuration =
     DotNet.BuildConfiguration.fromEnvironVarOrDefault "BuildConfiguration" DotNet.BuildConfiguration.Debug
 
@@ -16,8 +31,8 @@ Target.create "Clean" (fun _ ->
 )
 
 Target.create "GitVersion" (fun _ ->
-    Shell.Exec("gitversion", "/l console /output buildserver /updateAssemblyInfo")
-    |> ignore)
+    GitVersion.exec "/updateassemblyinfo" |> ignore
+)
 
 Target.create "DotNetBuild" (fun _ ->
     !! "**/*.*proj"
@@ -30,13 +45,11 @@ Target.create "Pack" (fun _ ->
             { opts with
                 Configuration = configuration
                 OutputPath = Some "../artifacts/Groomgy.HelloWorld"
-                VersionSuffix = Some ""
                 NoBuild = true
+                Common = { opts.Common with CustomParams = Some <| sprintf "/p:PackageVersion=%s" (GitVersion.semVer()) }
             })
         "./Groomgy.HelloWorld"
 )
-
-Target.create "Build" ignore
 
 Target.create "All" ignore
 
@@ -46,4 +59,4 @@ Target.create "All" ignore
   ==> "Pack"
   ==> "All"
 
-Target.runOrDefault "Build"
+Target.runOrDefault "All"
