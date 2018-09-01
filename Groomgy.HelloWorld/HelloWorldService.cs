9e9b252a16ea5e19a1f9a6a6a419431683a72577
Kimserey <kimserey.lam@groomgy.com>
2018-09-01 10:07:10 +0100

    Some commit

--- Groomgy.HelloWorld/HelloWorldService.cs
+++ Groomgy.HelloWorld/HelloWorldService.cs
@@ -7,11 +7,6 @@
             return "Hello World";
         }
 
-        public string SayMore()
-        {
-            return "Hello World More";
-        }
-
         public string GoodMorning()
         {
             return "Good morning";
