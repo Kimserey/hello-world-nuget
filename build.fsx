b187a490077a424f0edb736346b7a636378b36a1
Kimserey <kimserey.lam@groomgy.com>
2018-09-01 19:43:45 +0100

    execute gitversion after the repo is cleaned

--- build.fsx
+++ build.fsx
@@ -13,54 +13,64 @@ module Environment =
     let [<Literal>] BUILD_CONFIGURATION = "BuildConfiguration"
     let [<Literal>] REPOSITORY = "https://github.com/Kimserey/hello-world-nuget.git"
 
-type UpdateAssemblyInfo = unit -> ProcessResult
-type FullSemVer = string
-type NuGetVer = string
-
 module GitVersion =
     module Process =
         let exec f =
             Process.execWithResult f (System.TimeSpan.FromMinutes 2.)
 
     let private exec commit args =
-        Process.exec (fun info -> { info with FileName = "gitversion"; Arguments = sprintf "/url %s /b master /c %s %s" Environment.REPOSITORY commit args })
+        Process.exec (fun info -> { info with FileName = "gitversion"; Arguments = sprintf "/url %s /b master /dynamicRepoLocation .\gitversion /c %s %s" Environment.REPOSITORY commit args })
 
     let private getResult (result: ProcessResult) =
         result.Messages |> List.head
 
-    let (updateAssemblyInfo, fullSemVer, nuGetVer): UpdateAssemblyInfo * FullSemVer * NuGetVer =
-        let commit =
-            match Environment.environVarOrNone Environment.APPVEYOR_REPO_COMMIT with
-            | Some c -> c
-            | None -> Process.exec (fun info -> { info with FileName = "git"; Arguments = "rev-parse HEAD" }) |> getResult
+    let get =
+        let mutable value = None
 
-        printfn "Executing gitversion on detached HEAD. %s." commit
+        fun () ->
+            match value with
+            | None ->
+                let commit =
+                    match Environment.environVarOrNone Environment.APPVEYOR_REPO_COMMIT with
+                    | Some c -> c
+                    | None -> Process.exec (fun info -> { info with FileName = "git"; Arguments = "rev-parse HEAD" }) |> getResult
 
-        match Environment.environVarOrNone Environment.APPVEYOR_REPO_TAG_NAME with
-        | Some v ->
-            printfn "Full sementic versioning: '%s', NuGet sementic versioning: '%s'" v v
-            ((fun () -> exec commit "/updateassemblyinfo"), v, v)
-        | None ->
-            let fullSemVer = exec commit "/showvariable FullSemVer" |> getResult
-            let nuGetVer = exec commit "/showvariable NuGetVersionV2" |> getResult
-            printfn "Full sementic versioning: '%s', NuGet sementic versioning: '%s'" fullSemVer nuGetVer
-            ((fun () -> exec commit "/updateassemblyinfo"), fullSemVer, nuGetVer)
+                printfn "Executing gitversion on detached HEAD. %s." commit
+
+                match Environment.environVarOrNone Environment.APPVEYOR_REPO_TAG_NAME with
+                | Some v ->
+                    printfn "Full sementic versioning: '%s', NuGet sementic versioning: '%s'" v v
+                    value <- Some ((fun () -> exec commit "/updateassemblyinfo"), v, v)
+                | None ->
+                    let fullSemVer = exec commit "/showvariable FullSemVer" |> getResult
+                    let nuGetVer = exec commit "/showvariable NuGetVersionV2" |> getResult
+                    printfn "Full sementic versioning: '%s', NuGet sementic versioning: '%s'" fullSemVer nuGetVer
+                    value <- Some ((fun () -> exec commit "/updateassemblyinfo"), fullSemVer, nuGetVer)
+
+                Option.get value
+            | Some v -> v
 
 Target.create "Clean" (fun _ ->
     !! "**/bin"
     ++ "**/obj"
     ++ "**/artifacts"
+    ++ "gitversion"
+    |> Seq.map(fun x -> printfn "%s" x; x)
     |> Shell.cleanDirs
 )
 
 Target.create "PatchAssemblyInfo" (fun _ ->
-    GitVersion.updateAssemblyInfo()
+    let (updateAssemblyInfo, _, _) = GitVersion.get()
+
+    updateAssemblyInfo()
     |> fun res -> res.Messages
     |> List.iter (printfn "%s")
 )
 
 Target.create "UpdateBuildVersion" (fun _ ->
-    Shell.Exec("appveyor", sprintf "UpdateBuild -Version \"%s\"" GitVersion.fullSemVer)
+    let (_, fullSemVer, _) = GitVersion.get()
+
+    Shell.Exec("appveyor", sprintf "UpdateBuild -Version \"%s\"" fullSemVer)
     |> ignore
 )
 
@@ -70,6 +80,8 @@ Target.create "Build" (fun _ ->
 )
 
 Target.create "Pack" (fun _ ->
+    let (_, _, nuGetVer) = GitVersion.get()
+
     !! "**/Groomgy.*.*proj" -- "**/Groomgy.*Test.*proj"
     |> Seq.toList
     |> List.iter (fun proj ->
@@ -79,7 +91,7 @@ Target.create "Pack" (fun _ ->
                     Configuration = DotNet.BuildConfiguration.fromEnvironVarOrDefault Environment.BUILD_CONFIGURATION DotNet.BuildConfiguration.Debug
                     OutputPath = Some "../artifacts"
                     NoBuild = true
-                    Common = { opts.Common with CustomParams = Some (sprintf "/p:PackageVersion=%s" GitVersion.nuGetVer) }
+                    Common = { opts.Common with CustomParams = Some (sprintf "/p:PackageVersion=%s" nuGetVer) }
                 })
             proj
     )
