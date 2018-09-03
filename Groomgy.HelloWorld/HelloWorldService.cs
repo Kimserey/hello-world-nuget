138a6f41737be15f9a412c4e789661a762217048
Kimserey <kimserey.lam@groomgy.com>
2018-09-03 09:34:02 +0100

    Remove good night 2

--- Groomgy.HelloWorld/HelloWorldService.cs
+++ Groomgy.HelloWorld/HelloWorldService.cs
@@ -17,11 +17,6 @@
             return "Good night test";
         }
 
-        public string GoodNight2()
-        {
-            return "Good night";
-        }
-
         public string CallMyDependency()
         {
             return "My dependency: " + new HelloWorldDependencyLibrary.MyDependency().Get();
