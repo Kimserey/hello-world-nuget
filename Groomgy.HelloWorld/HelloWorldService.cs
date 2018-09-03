80f0cb0045933ccdf5ccbb9743e7ccd6a5ca08ae
Kimserey <kimserey.lam@groomgy.com>
2018-09-03 09:22:33 +0100

    Push on master

--- Groomgy.HelloWorld/HelloWorldService.cs
+++ Groomgy.HelloWorld/HelloWorldService.cs
@@ -22,6 +22,11 @@
             return "Good night";
         }
 
+        public string GoodNight2()
+        {
+            return "Good night";
+        }
+
         public string CallMyDependency()
         {
             return "My dependency: " + new HelloWorldDependencyLibrary.MyDependency().Get();
