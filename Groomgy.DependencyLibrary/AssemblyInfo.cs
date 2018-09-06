d43a43385a07c81e3b886892ce7e8a00a09893c6
Kimserey Lam <Kimserey@users.noreply.github.com>
2018-09-06 23:52:55 +0100

    Remove assemblyinfo and replace version update
    * Remove assembly info
    
    * Removed update assembly info
    
    * Added all versions and PackageId
    
    * Removed assembly info and updated directly version number from dotnet build
    
    * Replaced AssemblyVersion by FileVersion
    
    * Spelling mistake

--- Groomgy.DependencyLibrary/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: System.Reflection.AssemblyCompanyAttribute("Kimserey Lam")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
