5ab881d13bc2cab279f7681e7998695fa33f95c3
Kimserey <kimserey.lam@groomgy.com>
2018-09-11 09:59:09 +0100

    Remove test

--- Groomgy.HelloWorld/HelloWorldService.cs
+++ Groomgy.HelloWorld/HelloWorldService.cs
@@ -22,11 +22,6 @@
             return "Good evening";
         }
 
-        public string GoodEvening1()
-        {
-            return "Good evening 1";
-        }
-
         public string GoodNight()
         {
             return "Good night";
