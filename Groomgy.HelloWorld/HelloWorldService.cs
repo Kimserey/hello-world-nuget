2573c3e94df7602025c53c7612c948a6b6e8384a
Kimserey <kimserey.lam@groomgy.com>
2018-09-15 21:54:57 +0100

    Removed 3 and 4

--- Groomgy.HelloWorld/HelloWorldService.cs
+++ Groomgy.HelloWorld/HelloWorldService.cs
@@ -42,16 +42,6 @@
             return "Good night";
         }
 
-        public string GoodNight3()
-        {
-            return "Good night";
-        }
-
-        public string GoodNight4()
-        {
-            return "Good night";
-        }
-
         public string CallMyDependency()
         {
             return "My dependency: " + new HelloWorldDependencyLibrary.MyDependency().Get();
